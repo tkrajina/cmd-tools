22a9802038429b59e142ea57f1e1a3e58df2ea4e
tkrajina <tkrajina@gmail.com>
2011-11-25 08:38:02 +0100

    --quiet option to find-jar

--- utils/java.py
+++ utils/java.py
@@ -23,18 +23,24 @@ def get_jar_contents( jar_file ):
 
 	return result
 
-def find_in_jar( jar_file, class_string ):
-	print 'Searching %s' % jar_file
+def find_in_jar( jar_file, class_string, quiet = False ):
+	if not quiet:
+		print 'Searching %s' % jar_file
+
 	entries = get_jar_contents( jar_file )
 	for entry in entries:
 		if entry.lower().find( class_string.lower() ) >= 0:
 			print "Found %s in %s" % ( entry, jar_file )
-			answer = raw_input( 'Continue ( [y]es / [N]o / next [j]ar ) ?' )
-			answer = answer.lower()
-			if answer == 'n':
-				sys.exit( 0 )
-			elif answer == 'j':
-				return
-			else:
+
+			if quiet:
 				pass
+			else:
+				answer = raw_input( 'Continue ( [y]es / [N]o / next [j]ar ) ?' )
+				answer = answer.lower()
+				if answer == 'n':
+					sys.exit( 0 )
+				elif answer == 'j':
+					return
+				else:
+					pass
 
