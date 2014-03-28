904e8c1426651e12ef8d25e67a96260b546eeb5f
Tomo Krajina <tkrajina@gmail.com>
2014-03-28 20:49:09 +0100

    Workspace log, remove existing apps

--- deploy_on_eclipse_tomcat.sh
+++ deploy_on_eclipse_tomcat.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+set -e
+
 tomcat_instance=$1
 app_name=$2
+workspace_location=$3
+
+if [ -z "$workspace_location" ]
+then
+    workspace_location=".."
+    echo "No workspace location specified, using: $workspace_location"
+fi
 
 if [ -z "$tomcat_instance" ] || [ -z "$app_name" ]
 then
     echo "Usage:"
-    echo "deploy_on_eclipse_tomcat.sh tomcat_instance app_name"
+    echo "deploy_on_eclipse_tomcat.sh tomcat_instance app_name [workspace_location]"
     exit 1
 fi
 
@@ -14,14 +23,24 @@ if [ -d $tomcat_instance ]
 then
     webapps_path=$tomcat_instance
 else
-    webapps_path=../.metadata/.plugins/org.eclipse.wst.server.core/$tomcat_instance/webapps/
+    webapps_path=$workspace_location/.metadata/.plugins/org.eclipse.wst.server.core/$tomcat_instance/webapps/
 fi
+
+echo "Deploying to:$webapps_path"
 
 if [ ! -d "$webapps_path" ]
 then
     echo "Invalid path: $webapps_path"
     exit 1
 fi
+
+for app in $(ls $webapps_path)
+do
+    echo
+    echo "Removing $webapps_path/$app ?"
+    echo
+    rm --interactive=once -r -v $webapps_path/$app
+done
 
 mvn clean package -DskipTests=true || exit 1
 
